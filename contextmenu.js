21a39c0d785a489b2a2498e058046833705204dc
jakenester31 <111662560+jakenester31@users.noreply.github.com>
2025-04-27 16:19:15 -0700

    Hopefully these are the final changes
    -added child menu system
    -random bug fixes
    -remove menu id, (unneeded)
    -other

--- contextmenu.js
+++ contextmenu.js
@@ -1,33 +1,54 @@
 // General
-const defaultMenu = document.getElementById('menu1');
-var hover = 0;
 const allMenus = [];
-
+var hover = 0;
+const ms = [0,0];
+var defaultMenu;
+const settings = {state:1};
+const openChildren = [];
 
 //Create menus
 cmt('default menu');
-menuStart('menu1',"all");
-addButton('parent',0,'add','add')
+menuStart("all");
 addButton('Delete',0,'trash');
+addButton('parent',0,'add','add');
 addButton('undo','console.log("undo")','sm');
 addButton('download',0,'sm');
-addButton('save',0,'add','save');
+addButton('save',0,'add');
 
 cmt('test menu');
-menuStart('menu2',"img");
-addButton('parent',0,'add','add')
+menuStart("img");
+addButton('M2',0,'add','add')
 addButton('M2',0,'trash');
-addButton('undo','console.log("undo")','sm');
 addButton('download',0,'sm');
-addButton('save',0,'add','save');
+addButton('save',0,'add');
+
+menuStart('#add');
+addButton('save',0,'add');
 
 
 //functions
-function menuStart(name,selector) {
+
+// menu creation functions
+function menuStart(...selector) {
     const element = document.createElement('div');
-    element.id = name;
+    const sel = [];
+    for (var i = 0; i < selector.length; i++){
+        if (selector[i][0] == '#'){
+            sel.splice(i,1);
+            const tn = selector[i].slice(1);
+            const index = allMenus.find(e => 
+                e => e.classList.indexOf('ctm_' + tn) > -1
+            )
+            if (index != undefined){
+                element.id = 'ctmc_' + tn;
+            }
+        } else {
+            sel.push(selector[i]);
+        }
+    }
     element.className = "menu";
-    allMenus.push([element,selector]);
+    allMenus.push([element,sel]);
+    selector.includes('all') && (defaultMenu = element); //set default menu
     document.querySelector('contextMenu').appendChild(element);
 }
 
@@ -35,8 +56,11 @@ function addButton(txt,onclick,icon,parent){
     const element = document.createElement('button');
     icon != undefined && (element.classList.add("ico"),element.style.setProperty('--icon', 'url(menu-ico/' + icon +'.png)'));
     icon == 'sm' && element.style.setProperty('--icon', 'url(menu-ico/' + txt +'.png)');
-    parent != undefined && (element.classList.add("parent"), element.id = "ctm_" + parent);
-    element.innerText=txt;
+    const menu = document.querySelector('contextMenu').lastChild.id;
+    if (menu.slice(0,5) != "ctmc_") {
+        parent != undefined && element.classList.add("parent","ctm_" + parent);
+    }
+    element.innerHTML= '<p>' + txt + '</p>';
     element.onclick = new Function(onclick);
     document.querySelector('contextMenu').lastChild.appendChild(element);
 }
@@ -45,46 +69,126 @@ function cmt (txt) {
     document.querySelector('contextMenu').appendChild(document.createComment(txt));
 }
 
+// Child menu functions
+function childOpen(e) {
+    // get child menu
+    var id;
+    for (var i = 0; i < e.target.classList.length; i++){
+        if (e.target.classList[i].includes('ctm_')){
+            id = '#ctmc_' + e.target.classList[i].slice(4);
+            i = e.target.classList.length;
+        }
+    }
+    // child setup
+    const obj = document.querySelector(id);
+    openChildren.indexOf(id) == -1 && openChildren.push(id);
+    obj.classList.add('open');
+    // button info
+    const button = {
+        rect:e.target.getBoundingClientRect(),
+        right:e.target.getBoundingClientRect().right,
+        left:e.target.getBoundingClientRect().left - obj.clientWidth,
+    }
+    // new menu info
+    const target = {
+        height:obj.getBoundingClientRect().height,
+    }
+    // position
+    const pos = {x:0,y:0};
+    // x axis
+    pos.x = button.right;
+    if (pos.x + obj.clientWidth > document.documentElement.clientWidth){
+        pos.x = button.left;
+    }
+    // y axis
+    pos.y = button.rect.top - 10;
+    if (pos.y + target.height > document.documentElement.clientHeight){
+        pos.y = document.documentElement.clientHeight - target.height;
+    }
+    // set pos
+    obj.style.left = pos.x + 'px'
+    obj.style.top = pos.y + 'px'
+}
+
+function hideChildren(){
+    for (var i = 0; i < openChildren.length; i++){
+        const obj = document.querySelector(openChildren[i]);
+        obj.classList.remove('open');
+    }
+}
 
 //Listeners
 addEventListener('contextmenu', e => {
-    // prevent default menu
-    e.preventDefault();
-    // menu setup
-    try{menu.style.pointerEvents="none";} catch{}
-    const element = document.elementFromPoint(e.clientX, e.clientY).nodeName; //Element type
-    try{menu.classList.remove('open');} catch{}
-    menu = allMenus.find(newMenu=> newMenu[1] == 'all')[0]; //Default menu
-    const index = allMenus.find(newMenu=> newMenu[1].toUpperCase() == element.toUpperCase()); //Menu not... Default?
-    index != undefined && (menu = index[0]); //Set menu if not defualt
-    menu.style.pointerEvents='';
-    console.log(element);
-    menu.classList.add('open');
-    // menu position
-    // let x = e.clientX + menu.clientWidth > window.innerWidth ? e.clientX - menu.clientWidth : e.clientX;
-    let x = e.clientX + menu.clientWidth > window.innerWidth ? window.innerWidth - menu.clientWidth : e.clientX;
-    // let y = e.clientY + menu.clientHeight > window.innerHeight ? e.clientY - menu.clientHeight : e.clientY;
-    let y = e.clientY + menu.clientHeight > window.innerHeight ? window.innerHeight - menu.clientHeight : e.clientY;
-    menu.style.left = x + "px";
-    menu.style.top = y + "px";
+    // Is menu on?
+    if (['on',1].indexOf(settings.state) == -1){
+        return(0);
+    }
+    // old menu Guard
+    try{
+        menu.style.left = ''; //LEAVE!!!
+        menu.classList.remove('open'); //Your not that guy
+        menu = undefined;
+    } catch{} //If error, No old menu
+    // New menu
+    var element;
+    try {
+        // Info for new menu
+        element = document.elementFromPoint(e.clientX, e.clientY).nodeName; //Element type
+        menu = defaultMenu;
+        const index = allMenus.find(e => (e[1].find(
+            e => e.toUpperCase() == element.toUpperCase()
+        )))
+        index != undefined && (menu = index[0]);
+        if (menu == undefined) {
+            return(0)
+        }
+        // use google menu or custom menu?
+        ms.splice(0,1);
+        ms.push([e.clientX,e.clientY]);
+        if (ms[0].toString() != ms[1].toString()) {
+            e.preventDefault();
+            menu.classList.add('open'); //You are that guy
+        } 
+        
+        // Calc menu position
+
+        // set menu position
+        const mdm = [menu.clientWidth,menu.clientHeight], wdm = [window.innerWidth,window.innerHeight], mp = [e.clientX,e.clientY], pos = [];
+        for (var i = 0; i < 2; i++){
+            // calc
+            pos.push(mp[i] + mdm[i] > wdm[i] ? mp[i] - mdm[i] : mp[i]); // set menu position, standard context menu
+            pos[i] < 0 && (pos[i] = mp[i] + mdm[i] > wdm[i] ? mp[i] - mdm[i] / 2 : mp[i]); // custom: no space? no problem, just go mouse center
+            (pos[i] < 0 || pos[i] + mdm[i] > wdm[i]) && (pos[i] = wdm[i] / 2 - mdm[i] / 2); // custom: still no space? almost no problem, just go to screen center
+            // Result
+            menu.style[['left','top'][i]] = pos[i] + "px";
+        }
+    } catch(e){
+        if (typeof element == 'undefined'){
+            console.error('ContextMenu out of bounds');
+        }
+    }
 })
 
 // click in menu
 addEventListener('click', function(){
-    if (hover != 2 && menu.matches('.open')) {
-        menu.classList.remove('open');
-    }
+    try {
+        if (hover != 1 && menu.matches('.open')) {
+            menu.classList.remove('open');
+            hideChildren();
+        }
+    } catch {}
 })
 
 // click outside of menu
-addEventListener('mousedown', function(){
+addEventListener('mousedown', function(e){
     try{
         if (hover == 0 && menu.matches('.open')) {
             menu.classList.remove('open');
             document.querySelector('html').style.pointerEvents='none';
-            menu.style.pointerEvents='all';
+            hideChildren();
+            menu.style.pointerEvents='all'; // Not trash, Im important too! buttons need me!!!!!
         }
-    } catch{console.log('ignore md')}
+    } catch{}
 })
 
 addEventListener('mouseup', function(){
@@ -93,65 +197,40 @@ addEventListener('mouseup', function(){
     }
 })
 
-addEventListener('transitionend', e => {
-    const obj = e.target;
-    const op = getComputedStyle(obj).opacity;
-    if (op == 0){
-        obj.style.left = '-1000px';
-    }
-})
-
-
-//Hover detection
-// Enter menu
-for (var i = 0; i < allMenus.length; i++){
-    allMenus[i][0].addEventListener('mouseenter', function() {
-        // hover != 1 && console.log('enter');
-        hover = 1;
-    })
-}
-
-// Hover over menu
-{
-    const elements = document.getElementsByClassName('parent');
-    for (var i = 0; i < elements.length; i++){
-        elements[i].addEventListener('mouseleave', function() {
-            // hover != 1 && console.log('hover menu');
-            hover = 1;
-        })
-    }
-}
-
-// Hover over sub menu parent
-{
-    const elements = document.getElementsByClassName('parent');
-    for (var i = 0; i < elements.length; i++){
-        elements[i].addEventListener('mouseenter', function() {
-            // hover != 2 && console.log('hover parent');
-            hover = 2;
-        })
-    }
-}
-
-// leave menu
-for (var i = 0; i < allMenus.length; i++){
-    allMenus[i].onmouseleave = function() {
-        // hover != 0 && console.log('leave');
-        hover = 0;
-    }
-}
-
 addEventListener('wheel', function(){
     if (hover == 0){
         menu.classList.remove('open');
         // Hover Guard
         hover != 0 && console.log('leave');
         hover = 0;
+        hideChildren();
     }
 })
 
-setInterval(test,250)
+addEventListener('transitionend', e => {
+    const obj = e.target;
+    const op = getComputedStyle(obj).opacity;
+    if (op == 0){
+        obj.style.left = '';
+    }
+})
 
-function test() {
-    document.getElementById('box').innerHTML=hover;
+{
+    const elements = document.getElementsByClassName('parent');
+    // Hover over sub menu parent
+    for (var i = 0; i < elements.length; i++){
+        elements[i].addEventListener('mouseenter', e => {
+            hover = 1;
+            childOpen(e);
+        })
+        elements[i].addEventListener('mouseleave', function() {
+            hover = 0;
+        })
+    }
 }
+
+// for (var i = 0; i < allMenus.length; i++){
+//     allMenus[i][0].addEventListener('mouseleave', function() {
+//         hideChildren();
+//     })
+// }
